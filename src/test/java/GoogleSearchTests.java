a96cf6e48b472c5ea08ed84c8b45d788de3ac03c
mosetrova <mosetrova@solit-clouds.ru>
2023-05-25 22:46:04 +0300

    Initial commit

---
+++ src/test/java/GoogleSearchTests.java
@@ -0,0 +1,26 @@
+/*      1. Открыть google.com
+        2. Ввести "Selenide" в поле поиска
+        3. Проверить, что "selenide.org" появилось в результатах поиска
+*/
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byName;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class GoogleSearchTests {
+
+        @Test // при добавлении ниже появляется play
+        void selenideSearchTest () {
+            //1. Открыть google.com
+        open("https://www.google.com/");
+            //2. Ввести "Selenide" в поле поиска
+            //("name="q"") посомтрели из devtools
+    //    $("[name=q]").setValue("Selenide").pressEnter();
+            $("[name=q]").setValue("Selenide").pressEnter();
+            $("#search").shouldHave((text("selenide.org")));
+    }
+    }
+
+
